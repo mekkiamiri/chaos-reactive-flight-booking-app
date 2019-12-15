a589f0f82cc60bf1c13c7e9be5f437eb8ac66fcc
Karim Mehalebi <kmehalebi@egencia.com>
2019-12-15 17:46:55 +0100

    add chaos monkey

--- flight-shopping-service/src/main/kotlin/com/egencia/devoxx/flight/shopping/FlightShoppingServiceApplication.kt
+++ flight-shopping-service/src/main/kotlin/com/egencia/devoxx/flight/shopping/FlightShoppingServiceApplication.kt
@@ -1,11 +1,13 @@
 package com.egencia.devoxx.flight.shopping
 
+import com.oembedler.moon.graphql.boot.GraphQLJavaToolsAutoConfiguration
+import com.oembedler.moon.graphql.boot.GraphQLWebAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [GraphQLWebAutoConfiguration::class, GraphQLJavaToolsAutoConfiguration::class])
 class PnrAnalyserServiceApplication
 
-fun main(args: Array<String>) {
-	runApplication<PnrAnalyserServiceApplication>(*args)
+fun main() {
+	runApplication<PnrAnalyserServiceApplication>()
 }
